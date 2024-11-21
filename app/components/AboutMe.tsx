ca2f7af2abd9302db017a94fd929b7ecfe0e07d8
Hamad <hamadullah16@gmail.com>
2024-11-21 18:13:35 +0500

    charachters escaped

--- app/components/AboutMe.tsx
+++ app/components/AboutMe.tsx
@@ -1,19 +1,18 @@
-import React from 'react';
 
 function AboutMe() {
     return (
         <div className="md:px-14 sm:px-5 max-sm:px-5 flex flex-col gap-3">
             <p>
-                I’m a <strong>Software Engineer</strong> passionate about designing and building user-centric interfaces and exceptional user experiences. My journey in tech began as a Front-End Developer in 2022, where I struggled initially with color schemes, fonts, and design principles. Over time, I grew to appreciate these details, and they now play a key role in my work.
+                I&apos;m a <strong>Software Engineer</strong> passionate about designing and building user-centric interfaces and exceptional user experiences. My journey in tech began as a Front-End Developer in 2022, where I struggled initially with color schemes, fonts, and design principles. Over time, I grew to appreciate these details, and they now play a key role in my work.
             </p>
             <p>
-                In 2023, I enrolled in <strong>Meta’s Front-End Developer Specialization</strong>, sponsored by HEC. This program deepened my skills in <strong>React</strong>, <strong>Bootstrap</strong>, and <strong>CSS pre-processors</strong> through hands-on projects and rigorous assessments. Completing the specialization in six months was a rewarding challenge, capped by creating a portfolio that showcased what I’d learned. React unlocked my creativity—it showed me that frontend development is much more than "changing the color of a button"; it’s about crafting dynamic, interactive user interfaces with robust business logic.
+                In 2023, I enrolled in <strong>Metas Front-End Developer Specialization</strong>, sponsored by HEC. This program deepened my skills in <strong>React</strong>, <strong>Bootstrap</strong>, and <strong>CSS pre-processors</strong> through hands-on projects and rigorous assessments. Completing the specialization in six months was a rewarding challenge, capped by creating a portfolio that showcased what I&apos;d learned. React unlocked my creativity—it showed me that frontend development is much more than &quot;changing the color of a button&quot;; it&apos;s about crafting dynamic, interactive user interfaces with robust business logic.
             </p>
             <p>
-                After honing my React skills through personal projects, I discovered <strong>Next.js</strong>—a game-changer. At first, its concepts like static pages and server components were confusing. But as I dived deeper, I realized the brilliance of its approach to <strong>serverless architecture</strong>, <strong>SEO optimization</strong>, and <strong>blazing-fast performance</strong>. The idea of managing both client and server logic in a single repository fascinated me, and I haven’t looked back since.
+                After honing my React skills through personal projects, I discovered <strong>Next.js</strong>—a game-changer. At first, its concepts like static pages and server components were confusing. But as I dived deeper, I realized the brilliance of its approach to <strong>serverless architecture</strong>, <strong>SEO optimization</strong>, and <strong>blazing-fast performance</strong>. The idea of managing both client and server logic in a single repository fascinated me, and I haven&apos;t looked back since.
             </p>
             <p>
-                Currently, I’m focused on my <strong>Final Year Project</strong>, a <strong>Decentralized Electronic Voting Application</strong>. This project aims to make voting easier, more secure, and anonymous while reducing costs. It's a culmination of my skills and passion for building impactful applications.
+                Currently, I&apos;m focused on my <strong>Final Year Project</strong>, a <strong>Decentralized Electronic Voting Application</strong>. This project aims to make voting easier, more secure, and anonymous while reducing costs. It&apos;s a culmination of my skills and passion for building impactful applications.
             </p>
         </div>
     );
