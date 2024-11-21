41ab2686a392878603f66388206d603f83e4621e
Hamad <hamadullah16@gmail.com>
2024-11-21 19:50:46 +0500

    unnecessary imports removed

--- app/components/WorkCard.tsx
+++ app/components/WorkCard.tsx
@@ -1,6 +1,6 @@
 'use client'
 import Link from 'next/link';
-import React, { useEffect, useState } from 'react'
+import React, { useState } from 'react'
 import { Button } from '../../components/ui/button';
 import { Badge } from '../../components/ui/badge';
 import { LinkIcon } from 'lucide-react';
