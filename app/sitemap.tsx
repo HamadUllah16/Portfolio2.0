c6a5e03aafae58cdacfc8671978cd4e697dfc0d8
Hamad Ullah <hamadullah16@gmail.com>
2025-02-27 00:14:32 +0500

    fixing sitemap

--- app/sitemap.tsx
+++ app/sitemap.tsx
@@ -1,7 +1,7 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-    const baseUrl = "https://hamad-portfolio.vercel.app/";
+    const baseUrl = "https://hamad-portfolio.vercel.app";
     return [
         {
             url: `${baseUrl}/`,
