c8a23498af080666dfdde8b189de53d3a12ce6ec
Hamad Ullah <hamadullah16@gmail.com>
2025-02-27 00:11:00 +0500

    sitemap generated

---
+++ app/sitemap.tsx
@@ -0,0 +1,23 @@
+import { MetadataRoute } from "next";
+
+export default function sitemap(): MetadataRoute.Sitemap {
+    const baseUrl = "https://hamad-portfolio.vercel.app/";
+    return [
+        {
+            url: `${baseUrl}/`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${baseUrl}/experience`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${baseUrl}/about`,
+            lastModified: new Date().toISOString(),
+        },
+        {
+            url: `${baseUrl}/freelance`,
+            lastModified: new Date().toISOString(),
+        },
+    ];
+}
